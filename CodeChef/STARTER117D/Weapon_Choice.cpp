6dfb7b591b0b9d320826b1b97884f4e6ef31ad46
kalra <hkalra0001@gmail.com>
2024-01-17 21:50:08 +0530

    solved two questions

--- CodeChef/STARTER117D/Weapon_Choice.cpp
+++ CodeChef/STARTER117D/Weapon_Choice.cpp
@@ -1,46 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-// macros 
-#define endl '\n'
-#define pb push_back
-
-// function macros
-
-#define all(x) x.begin(),x.end()
-
-void solve(int tt)
+int solve(int tt)
 {
-  // your code goes here
-  int health,x,y1,y2,k;
-  cin>>health>>x>>y1>>y2>>k;
-  int temp=health;
-  int first_weapon=0;
-  int second_weapon=0;
-  while (health>0)
-  {
-    first_weapon++;
-    health-=x;
-  }
-  while (temp>0)
-  {
-    second_weapon++;
-    if (k>0) temp-=y1;
-    else temp-=y2;
-    k--;
-  }
-  if (first_weapon>second_weapon) cout<<second_weapon<<endl;
-  else cout<<first_weapon<<endl;
+    int health,x,y1,y2,k,a,b,c;
+    cin>>health>>x>>y1>>y2>>k;
+    if (health%x==0) a=health/x;
+    else a=health/x + 1;
+    c=health-k*y1;
+    if (c<=0)
+    {
+        if (health%y1==0) b=health/y1;
+        else b=health/y1+1;
+
+    } 
+    else 
+    {
+        if (c%y2==0)
+        {
+            b=k+c/y2;
+
+        }
+        else 
+        {
+            b=k+c/y2 +1 ;
+        }
+    }
+    if (a>b) return b;
+    else return a;
 }
 int32_t main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  int testcases=1;
-  cin>>testcases;
-  for (int tt=1;tt<=testcases;tt++)
-  {
-    solve(tt);
-  }
-  return 0;
+    int testcases;
+    cin>>testcases;
+    for (int tt=1;tt<=testcases;tt++)
+    {
+        cout<<solve(tt)<<endl;
+    }
+    return 0;
 }
