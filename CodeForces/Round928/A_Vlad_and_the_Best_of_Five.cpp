a53c2f279a4d667eb4723b799d848e2b536659b1
kalra <hkalra0001@gmail.com>
2024-02-19 21:13:51 +0530

    worst contest

---
+++ CodeForces/Round928/A_Vlad_and_the_Best_of_Five.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// tYPEDEFs
+
+#define int long long
+#define vInt vector<int>
+#define vStr vector<string>
+
+// FUNCTION MACROS
+
+#define endl '\n'
+#define all(x) x.begin(),x.end()
+#define allr(x) x.rbegin(),x.rend();
+#define pb push_back
+
+
+void fastInit()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+// Flags to use: -std=c++17 -fsanitize=address -fsanitize=undefined -o <file_name_without_extension>/
+//////////////////////////////////////CODE HERE//////////////////////////////////////////////////////
+
+
+void solve(int tt)
+{
+
+    string str;
+    cin>>str;
+    int a=0;
+    int b=0;
+    for (int i=0;i<5;i++)
+    {
+        if (str[i]=='A') a++;
+        else b++;
+    }
+    if (a>b)
+    {
+        cout<<"A"<<endl;
+    }
+    else 
+    {
+        cout<<"B"<<endl;
+    }
+
+}
+
+
+int32_t main()
+{
+    fastInit();
+    #ifdef LOCAL
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    int testcases=1;
+    cin>>testcases;
+    for (int tt=1;tt<=testcases;tt++)
+    {
+
+        solve(tt);
+
+    }
+    return 0;
+}
